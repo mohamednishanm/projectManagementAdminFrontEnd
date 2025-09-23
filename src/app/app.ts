4ee068ca3987806ebef1a79ff148ac9b73f3fb48
Fuhad Saneen K <fuhad.k@experionglobal.local>
2025-09-23 10:34:20 +0530

    new component created - form details adding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Milestone } from './milestone/milestone';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Milestone],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/milestone/milestone.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-milestone',
+  imports: [],
+  templateUrl: './milestone.html',
+  styleUrl: './milestone.css'
+})
+export class Milestone {
+
+}
