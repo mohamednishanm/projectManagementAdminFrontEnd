abb13c93445b8d243a471e86addb14fb34750b4b
Aashin Thomas <aashinthomas072@gmail.com>
2025-09-23 10:38:03 +0530

    Initial Commit

---
+++ src/app/add-team-members/add-team-members.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-team-members',
+  imports: [],
+  templateUrl: './add-team-members.html',
+  styleUrl: './add-team-members.css'
+})
+export class AddTeamMembers {
+
+}
